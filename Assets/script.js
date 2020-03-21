b4d31f8a121c2f3c22c210dde3e8c1c90706bda6
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-03-20 21:54:20 -0400

    Password object, setCriteria function

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,34 @@
+
+
+var passwordObj = {
+  passLength: 0,
+  includeSymbols: false,
+  includeNumbers: false,
+  includeLowerCase: false,
+  includeUpperCase: false,
+
+};
+
+
+
+function setCriteria() {
+
+  this.passLength = prompt("Enter password length");
+  this.includeSymbols = confirm("Do you want to include symbols?");
+  this.includeNumbers = confirm("Do you want to include numbers?");
+  this.includeLowerCase = confirm("Do you want to include lowercase letters?");
+  this.includeUpperCase = confir
+
+  
+
+
+}
+
+
+
+// 0-9 == numbers, 9-35 == lowercase letters, 36-61 == uppercase, 62 -92 ==symbols
+var availChar = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,6 +40,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+generatePassword(){
+
+
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
